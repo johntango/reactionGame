3d1e2c7e3d4113bc0a3d7f8f18de60af6148b399
John <jrw@mit.edu>
2022-02-01 15:21:04 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# reactionTest - author jrw@mit.edu
+## Test the users reaction time to click on a button
+## Based on F1 lights-go-out to start car races
+
+
---
+++ server.js
@@ -0,0 +1,33 @@
+let express = require('express');
+let bodyParser = require('body-parser');
+let path = require('path');
+let app = express();
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true }));
+
+let data = [99999];
+
+let root = path.resolve(__dirname) + '/public/';
+app.get('/', (req, res) => {
+    res.sendFile('block.html', { 'root': root });
+});
+app.get('/set', function(req, res) {
+    
+    x = parseInt(req.query.x)
+    data.push(x);
+    fastest = Math.min(...data);
+    console.log("fastest:" + fastest);
+    console.log("data:" + data);
+});
+app.get('/get', function(req, res) {
+    res.send(JSON.stringify(data));
+});
+app.get('/fastest',function(req,res){
+    res.json(fastest)
+})
+app.get('/pac', function(req, res) {
+    res.sendFile('/PacMan' + req.query.id + '.png', { 'root': root });
+});
+
+app.listen(3000);
+console.log("Running on port 3000");
