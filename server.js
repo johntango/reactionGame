6217ecc6354c9007f991834100efa424fd89b6c3
John <jrw@mit.edu>
2022-02-02 16:42:31 -0500

    test5

--- server.js
+++ server.js
@@ -6,18 +6,18 @@ app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true }));
 
 let data = [99999];
-
+let fastest = 9999;
 let root = path.resolve(__dirname) + '/public/';
 app.get('/', (req, res) => {
     res.sendFile('block.html', { 'root': root });
 });
 app.get('/set', function(req, res) {
-    
     x = parseInt(req.query.x)
     data.push(x);
     fastest = Math.min(...data);
     console.log("fastest:" + fastest);
     console.log("data:" + data);
+    res.json(fastest)
 });
 app.get('/get', function(req, res) {
     res.send(JSON.stringify(data));
@@ -25,9 +25,6 @@ app.get('/get', function(req, res) {
 app.get('/fastest',function(req,res){
     res.json(fastest)
 })
-app.get('/pac', function(req, res) {
-    res.sendFile('/PacMan' + req.query.id + '.png', { 'root': root });
-});
 
 app.listen(3000);
 console.log("Running on port 3000");
